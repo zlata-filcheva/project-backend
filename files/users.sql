bf8ab138040d45752f48057611b1b7b4cf31c05d
Igor Filchev <90631197+filchev-igor@users.noreply.github.com>
2024-05-01 22:09:38 +0300

    5 - SQL files were updated (#5)

--- files/users.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `users` (`password`, `email`, `role`) VALUES
-                                                      ('password1', 'user1@example.com', 'user'),
-                                                      ('password2', 'user2@example.com', 'user'),
-                                                      ('password3', 'user3@example.com', 'user'),
-                                                      ('password4', 'user4@example.com', 'user'),
-                                                      ('password5', 'user5@example.com', 'user');
