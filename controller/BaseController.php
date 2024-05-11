6ef1a76bfcf9105b17603368f981177567c3ba02
Igor Filchev <90631197+filchev-igor@users.noreply.github.com>
2024-05-11 06:18:41 +0300

    10 CategoryController.php was corrected for raw post data (#10)

--- controller/BaseController.php
+++ controller/BaseController.php
@@ -7,13 +7,7 @@ class BaseController
     const FRONT_END_URI = "https://127.0.0.1:5173";
 
     const HEADERS_200 = [
-        "Content-Type: application/json",
         "HTTP/1.1 200 OK",
-        "Access-Control-Allow-Origin: " . BaseController::FRONT_END_URI,
-        "Access-Control-Allow-Methods: GET, PATCH, DELETE",
-        "Access-Control-Allow-Headers: Content-Type",
-        "Access-Control-Allow-Credentials: true",
-        "Access-Control-Max-Age: 86400"
     ];
 
     const RESPONSE_DATA_DECODED_422 = ['error' => 'Method not supported'];
@@ -80,10 +74,9 @@ class BaseController
             : '';
 
         return [
-            "Content-Type: application/json",
             "HTTP/1.1 201 Created",
             $locationHeader,
-            "Cache-Control: no-cache"
+            "Cache-Control: no-cache",
         ];
     }
 
