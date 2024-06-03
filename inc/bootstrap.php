474be164c9f85354f1123692cfdcc965410b0853
Igor Filchev <filchev.igor@gmail.com>
2024-06-04 01:56:12 +0300

    Some changes

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -8,6 +8,6 @@ require PROJECT_ROOT_PATH . "/controller/BaseController.php";
 
 require PROJECT_ROOT_PATH . "/model/CategoryModel.php";
 require PROJECT_ROOT_PATH . "/model/CommentModel.php";
-require PROJECT_ROOT_PATH . "/model/PostModel.php";
+//require PROJECT_ROOT_PATH . "/model/PostModel.php";
 require PROJECT_ROOT_PATH . "/model/TagModel.php";
 require PROJECT_ROOT_PATH . "/model/UserModel.php";
