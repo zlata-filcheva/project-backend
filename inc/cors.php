81085cd0d3f3dd87badce5869a9de9e19905cc34
Igor Filchev <filchev.igor@gmail.com>
2024-06-04 04:00:14 +0300

    Some changes

--- inc/cors.php
+++ inc/cors.php
@@ -3,11 +3,11 @@
 $hasDevelopmentMode = $_SERVER['SERVER_NAME'] === '127.0.0.1';
 
 if (!$hasDevelopmentMode) {
-    //return;
+    return;
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: https://127.0.0.1:5173');
     header('Access-Control-Allow-Methods: GET, POST, DELETE, PUT, PATCH, OPTIONS');
     header('Access-Control-Allow-Headers: token, Content-Type');
     header('Access-Control-Max-Age: 86400');
