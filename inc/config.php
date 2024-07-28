35c8467dae79551cb1cc5b6d20e1a5c70e3605e4
Igor Filchev <filchev.igor@gmail.com>
2024-07-28 23:29:12 +0300

    test backend

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,5 @@
 <?php
-define("DB_HOST", "http://139.59.156.223:8080");
+define("DB_HOST", "http://139.59.156.223");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
 define("DB_DATABASE_NAME", "newReddit");
